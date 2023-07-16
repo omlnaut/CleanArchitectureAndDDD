30c4f71eb43007262549694e69ac75b2b326a54d
omlnaut <oneironaut.oml@gmail.com>
2023-07-16 22:55:24 +0200

    jwt options setup

--- BuberDiner/BuberDiner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDiner/BuberDiner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using BuberDiner.Application.Common.Interfaces.Authentication;
 using BuberDiner.Application.Common.Interfaces.Services;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace BuberDiner.Infrastructure.Authentication;
@@ -10,16 +11,18 @@ namespace BuberDiner.Infrastructure.Authentication;
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly JwtSettings _jwtSettings;
 
-    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtSettingsOptions)
     {
         _dateTimeProvider = dateTimeProvider;
+        _jwtSettings = jwtSettingsOptions.Value;
     }
 
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
         var signingCredentials = new SigningCredentials(
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key")),
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
             SecurityAlgorithms.HmacSha256
         );
         var claims = new[]
@@ -31,8 +34,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         };
 
         var securityToken = new JwtSecurityToken(
-            issuer: "BuberDiner",
-            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpirationInMinutes),
             claims: claims,
             signingCredentials: signingCredentials
         );
--- BuberDiner/BuberDiner.Infrastructure/DependencyInjection.cs
+++ BuberDiner/BuberDiner.Infrastructure/DependencyInjection.cs
@@ -2,14 +2,18 @@ using BuberDiner.Application.Common.Interfaces.Authentication;
 using BuberDiner.Application.Common.Interfaces.Services;
 using BuberDiner.Infrastructure.Authentication;
 using BuberDiner.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDiner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
