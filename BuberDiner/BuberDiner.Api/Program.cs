dded75b85ee23872d7fa2fe51ccc18f4f7cb569f
Oliver Rüger <oliver.rueger@relaxdays.de>
2023-07-21 10:08:45 +0200

    with filter attribute

--- BuberDiner/BuberDiner.Api/Program.cs
+++ BuberDiner/BuberDiner.Api/Program.cs
@@ -1,4 +1,4 @@
-using BuberDiner.Api.Middleware;
+using BuberDiner.Api.Filters;
 using BuberDiner.Application;
 using BuberDiner.Infrastructure;
 
@@ -7,13 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 {
     _ = builder.Services.AddApplication();
     _ = builder.Services.AddInfrastructure(builder.Configuration);
-    _ = builder.Services.AddControllers();
+    _ = builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 
 {
-    _ = app.UseMiddleware<ErrorHandlingMiddleware>();
+    // _ = app.UseMiddleware<ErrorHandlingMiddleware>();
     _ = app.UseHttpsRedirection();
     _ = app.MapControllers();
     app.Run();
